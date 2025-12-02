2a34b8b492bd21e022586afe248d8d81c89a1260
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-03 07:14:54 +1100

    functional LinkedList methods

--- linked-list.rb
+++ linked-list.rb
@@ -4,8 +4,8 @@ class LinkedList
   attr_accessor :name
 
   def initialize
-    @head = Node.new
-    @tail = Node.new
+    @head = nil
+    @tail = nil
   end
 
   def prepend(value)
@@ -23,7 +23,7 @@ class LinkedList
   def append(value)
     node = Node.new
     node.value = value
-    if @head.value.nil?
+    if @head.nil?
       @head = node
       @tail = node
     else 
@@ -45,10 +45,12 @@ class LinkedList
   end
 
   def head
+    return nil if @head.nil?
     @head.value
   end
 
   def tail
+    return nil if @head.nil?
     @tail.value
   end
 
@@ -65,7 +67,7 @@ class LinkedList
   end
 
   def pop
-    nil if @head.nil?
+    return nil if @head.nil?
     
     prev2 = @head
     prev = prev2.next_node
@@ -83,6 +85,32 @@ class LinkedList
     data
   end
 
+  def contains(value)
+    current = @head
+
+    while current.value != value
+      current = current.next_node
+
+      break if current.next_node.nil?
+    end
+
+    current.value == value ? true : false
+  end
+
+  def find(value)
+    current = @head
+    index = 0
+
+    while !current.nil?
+      break if current.value == value
+
+      current = current.next_node
+      index += 1
+    end
+
+    index unless current.nil?
+  end
+
   def to_s
     string = ''
     current = @head
@@ -95,5 +123,43 @@ class LinkedList
     string << "nil"
 
     string
+  end
+
+  def insert_at(value, index)
+    return self.prepend(value) if index == 0 || @head.nil?
+    return self.append(value) if index >= self.size
+
+    current = @head
+    count = 0
+
+    while count < index - 1
+      current = current.next_node
+      count += 1
+    end
+
+    new_node = Node.new
+    new_node.value = value
+    
+    new_node.next_node = current.next_node
+    current.next_node = new_node
+
+  end
+
+  def remove_at(index)
+    self.pop if index >= self.size
+
+    current = @head
+    count = 0
+
+    while count < index - 1
+      current = current.next_node
+      count += 1
+    end
+    
+    del_node = current.next_node
+
+    current.next_node = del_node.next_node
+
+    del_node.value
   end
 end
--- main.rb
+++ main.rb
@@ -2,15 +2,18 @@ require './linked-list'
 
 list = LinkedList.new
 
-list.append('dog')
-list.append('cat')
-list.append('parrot')
-list.append('hamster')
-list.append('snake')
-list.append('turtle')
+#  list.append('dog')
+#  list.append('cat')
+#  list.append('parrot')
+#  list.append('hamster')
+#  list.append('snake')
+#  list.append('turtle')
 
 p list.head
 p list.tail
-p list.pop
-p list.tail
 p list.to_s
+list.insert_at('chicken', 3)
+p list.to_s
+puts list.remove_at(3)
+p list.to_s
+p list.size
